281cfd8ef93c3d10370379f45bd4aa0f9df5974f
Douglas Deodato <douglasdeodato@gmail.com>
2016-05-09 23:04:49 +0100

    old update

--- SITE/public/js/main.js
+++ SITE/public/js/main.js
@@ -1,6 +1,48 @@
-$(function() {
+// $(function() {
+
+// new WOW().init();
+
+
+// });
+
+
+
+// $(window).scroll(function(){
+
+// var wScroll = $(this).scrollTop();
+
+// console.log(wScroll)
+
+// });
 
 new WOW().init();
-
-
+var fixed = false;
+// These are the classes when the animation is processing.
+var animateGroup = 'wow shake animated';
+$(document).scroll(function() {
+    if( $(this).scrollTop() >= 350 ) {
+        if( !fixed ) {
+            fixed = true;
+            // Make them show.
+            // $('footer')
+            //     .addClass(animateGroup)
+            //     .css({visibility: 'visible', 'animation-name': 'shake'}); 
+          
+            // Demo without wow.js
+            $('footer').addClass('animated shake').show();
+        }
+    } else {
+        if( fixed ) {
+            fixed = false;
+            // Remove anything we just added.
+            // $('footer')
+            //     .removeClass(animateGroup)
+            //     .removeAttr('style');
+                // Or if you have other styles, use below to remove specific style instead.
+                //.css({visibility: '', 'animation-name': ''});
+          
+          // Demo without wow.js
+          //$('footer').hide();
+        }
+    }
 });
