740c5523b5c48c1ccf66dd0edbd9bb1636aa9ab9
Elias Moreira <elias_moreirajunior@mastercard.com>
2016-10-11 23:05:50 +0100

    move pages, add new server file

---
+++ SITE/server.js
@@ -0,0 +1,26 @@
+/**
+ * Created by eliasmj on 11/10/2016.
+ */
+
+(function () {
+    'use strict'
+
+    var express = require('express');
+    var app = express();
+
+    var path    = require("path");
+
+    app.use(express.static('app'));
+    app.use(express.static('public'));
+
+    app.get('/', function (req, res) {
+        //res.send('Hello World!');
+        res.sendFile(path.join(__dirname+'/test.html'));
+    });
+
+
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!');
+    });
+
+})();
